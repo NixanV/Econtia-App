21ca676502fbc30dcbcbdc5c4a04f08444a1a582
Nikolay Petrov <nikip1303@gmail.com>
2024-06-28 16:44:59 +0300

    onLogin submit created

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,30 @@
 import { Route, Routes } from "react-router-dom";
 import { Login } from "./components/Login/Login";
 import { AuthContext } from "./contexts/AuthContext";
+import { useState } from "react";
+
+import {login} from "./services/authService"
 
 function App() {
 
+  const [auth, setAuth] = useState({});
+
+  const onLoginSubmit = async (data) => {
+    try {
+      const res = await login(data);
+      setAuth(res)
+    } catch (error) {
+      console.log("Problem");
+    }
+  }
 
   const contextValues = {
-
+    onLoginSubmit,
   };
   return (
     <div className="App">
       <main> 
-        <AuthContext.Provider>
+        <AuthContext.Provider value={contextValues}>
           <Routes>
             <Route path="/" element={<Login />}/>
           </Routes>
