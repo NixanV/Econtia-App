fe08c812c4e3892774f55d29af4acfac879175c4
Nikolay Petrov <nikip1303@gmail.com>
2024-07-05 11:26:24 +0300

    changed form for nov tovarene

--- client/src/components/NovTovarene/NovTovarene.js
+++ client/src/components/NovTovarene/NovTovarene.js
@@ -1,29 +1,36 @@
-import style from "./novTovarene.module.css"
+import style from "./novTovarene.module.css";
+import {Button, Form} from "react-bootstrap";
+
 
 export const NovTovarene = () => {
 
     return(
-        <section className={style["main-section"]}>
-            <div className={style["registration-form"]}>
-	            <div className={style['fieldset']}>
-                    <legend>Wanna be Cool?!</legend>
-                        <form method="post" >
-                            <div className={style['row']}>
-                                <label >Номер на линията: </label>
-                                <input type="text"  name='numberOfLine' data-required="true" />
-                            </div>
-                            <div className={style['row']}>
-                                <label>Име на линията: </label>
-                                <input type="text"  name='nameOfLine' data-required="true" />
-                            </div>
-                            <div className={style['row']}>
-                                <label>Шофьор: </label>
-                                <input type="text" name='driver' data-required="true" />
-                            </div>
-                            <input type="submit" value="Confirm" />
-                        </form>
-                    </div>
-                </div>
-        </section>
+        <Form>
+            <Form.Group className={style["mb-3"]} controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Text className={style["text-muted"]}>
+                We'll never share your email with anyone else.
+                </Form.Text>
+            </Form.Group>
+
+            <Form.Group className={style["mb-3"]} controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className={style["mb-3"]} controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Form.Group className={style["mb-3"]} controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+            <Form.Group className={style["mb-3"]} controlId="formBasicCheckbox">
+                <Form.Check type="checkbox" label="Check me out" />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+                Submit
+            </Button>
+        </Form>
     )
 }
