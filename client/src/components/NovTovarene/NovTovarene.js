7e99776f33632b43319d7a158a616f020ce6e063
Nikolay Petrov <nikip1303@gmail.com>
2024-07-05 11:28:45 +0300

    updated form

--- client/src/components/NovTovarene/NovTovarene.js
+++ client/src/components/NovTovarene/NovTovarene.js
@@ -7,27 +7,20 @@ export const NovTovarene = () => {
     return(
         <Form>
             <Form.Group className={style["mb-3"]} controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className={style["text-muted"]}>
-                We'll never share your email with anyone else.
-                </Form.Text>
+                <Form.Label>Номер на линията: </Form.Label>
+                <Form.Control type="text" />
             </Form.Group>
 
             <Form.Group className={style["mb-3"]} controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Label>Име на линията: </Form.Label>
+                <Form.Control type="text"/>
             </Form.Group>
-            <Form.Group className={style["mb-3"]} controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
+            
             <Form.Group className={style["mb-3"]} controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
+                <Form.Label>Шофьор: </Form.Label>
+                <Form.Control type="text"/>
             </Form.Group>
-            <Form.Group className={style["mb-3"]} controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
+            
             <Button variant="primary" type="submit">
                 Submit
             </Button>
