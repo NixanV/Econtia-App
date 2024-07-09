f0999d19a1d26701699b534f6769d65c291623e7
Nikolay Petrov <nikip1303@gmail.com>
2024-07-09 22:16:41 +0300

    search grouping created useState and useEffect

--- client/src/components/AllGroupingFolders/SearchGrouping/SearchGrouping.js
+++ client/src/components/AllGroupingFolders/SearchGrouping/SearchGrouping.js
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import { getAllGrouping } from "../../../services/groupingService";
+
+
+
 export const SearachingGrouping = () => {
+
+    const [lines, setLines] = useState([]);
+
+    useEffect(() => {
+        getAllGrouping().then(result => {
+            setLines(Object.values(result))
+        })
+    }, [])
+
+
     return(
-        <p>
-            hello again me
-        </p>
+        <section className={style["section-t-grupirane"]}>
+        {!lines ? 
+        <div>
+            no lines existed
+        </div> : lines.map((line) => (
+            <TovareneComponent key={line._id} {...line} />
+        ))}
+    </section>
     )
 }
