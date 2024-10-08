cc58ff6e3999faa9e40238f14f8299a558662068
Nikolay Petrov <nikip1303@gmail.com>
2024-10-08 14:54:19 +0300

    created service for getting one from grouping

--- client/src/services/groupingService.js
+++ client/src/services/groupingService.js
@@ -10,3 +10,14 @@ export const getAllGrouping = async () => {
         console.log("error in receiving data");
     }
 }
+
+export const getOneGrouping = async( id) =>{
+    try{
+        const response  = await fetch(`${baseUrl}/${id}`)
+        const result = await response.json();
+        return result;
+    }
+    catch (error ){
+        console.log("error in receiving right data");
+    }
+}
