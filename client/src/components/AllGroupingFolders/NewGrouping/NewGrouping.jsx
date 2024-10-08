fbb142b5cb05464573883c41dc97cbfb166d284a
Nikolay Petrov <nikip1303@gmail.com>
2024-10-08 17:42:02 +0300

    submithandler and changehandler created

--- client/src/components/AllGroupingFolders/NewGrouping/NewGrouping.jsx
+++ client/src/components/AllGroupingFolders/NewGrouping/NewGrouping.jsx
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import style from "./newgrouping.module.css";
 import {Button, Form} from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { postNewGrouping } from "../../../services/groupingService";
 
 export const NewGrouping = () => {
 
-    
+    const navigate = useNavigate();
 
-    const onSubmitHandler = () => {
-        return null;
+    const [data, setData] = useState({
+        numberOfLine: '',
+        nameOfCity: '',
+        numberOfPackeges: '',
+    })
+
+    const onSubmitHandler = async (e, data) => {
+        e.preventDefault();
+        const res = await postNewGrouping(data);
+        navigate(`/grupirane/${res._id}`, {replace:true})
     }
 
-    const changeHandler = () => {
-        return null;
+    const changeHandler = (e) => {
+        setData(state => ({
+            
+            ...state,
+            [e.target.name]: [e.target.value]
+        })
+    )
     }
 
     const clickingSubmit = () => {
@@ -19,7 +34,7 @@ export const NewGrouping = () => {
     }
 
     return(       
-        <Form onSubmit={(e) => onSubmitHandler(e)} className={style["form-nov-tovarene"]}>
+        <Form onSubmit={(e) => onSubmitHandler(e, data)} className={style["form-nov-tovarene"]}>
             <Form.Group className={style["mb-3"]} controlId="formBasicEmail">
                 <Form.Label className={style["labels-nov-tovarene"]}>Номер на линията: </Form.Label>
                 <Form.Control
@@ -27,31 +42,31 @@ export const NewGrouping = () => {
                     type="text"
                     name="numberOfLine"
                     onChange={(e) => changeHandler(e)}
-                    //value={data.numberOfLine}
+                    value={data.numberOfLine}
                     required
                 />
             </Form.Group>
 
             <Form.Group className={style["mb-3"]} controlId="formBasicPassword">
-                <Form.Label className={style["labels-nov-tovarene"]}>Име на линията: </Form.Label>
+                <Form.Label className={style["labels-nov-tovarene"]}>Населено място: </Form.Label>
                 <Form.Control 
                     className={style["textarea-nov-t"]}
                     type="text"
                     name="nameOfLine"
                     onChange={(e) => changeHandler(e)}
-                    //value={data.nameOfLine}
+                    value={data.nameOfLine}
                     required
                 />
             </Form.Group>
 
             <Form.Group className={style["mb-3"]} controlId="formBasicPassword">
-                <Form.Label className={style["labels-nov-tovarene"]}>Шофьор: </Form.Label>
+                <Form.Label className={style["labels-nov-tovarene"]}>Брой пратки: </Form.Label>
                 <Form.Control
                     className={style["textarea-nov-t"]}
                     type="text"
                     name="driver"
                     onChange={(e) => changeHandler(e)}
-                    //value={data.driver}
+                    value={data.driver}
                     required
                 />
             </Form.Group>
